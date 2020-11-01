1710b0fa0aa6ec87f24ba9d231e3baee8f72ab70
Naasgul <lnaas@hotmail.de>
2020-11-01 17:21:26 +0100

    Basic setup for react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./global.css";
+import Dropdown from "./components/Dropdown";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Unsere tolle Movie-App</h1>
+      <Dropdown />
     </div>
   );
 }
